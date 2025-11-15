fa48dcd1786d83ba075f094b697c11002fb1cd26
sergio <sergioandresherreravallejo@gmail.com>
2025-11-15 12:33:58 -0500

    a

--- proyecto-03/probar_boton.py
+++ proyecto-03/probar_boton.py
@@ -1,11 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setmode(GPIO.BCM)  # usar numeración BCM
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # pull-down
 
-print("Prueba: presiona el botón")
+print("Probando botón en GPIO 27 (PULL-DOWN).")
+print("Suelta = 0 | Presiona = 1")
 
-while True:
-    print(GPIO.input(27))
-    time.sleep(0.2)
+try:
+    while True:
+        print(GPIO.input(27))
+        time.sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
