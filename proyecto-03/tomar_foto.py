2a96abcbf4fa7c344f8d3057f907f0e574b3d2e3
sergio <sergioandresherreravallejo@gmail.com>
2025-11-15 12:04:51 -0500

    prueba botón

--- proyecto-03/tomar_foto.py
+++ proyecto-03/tomar_foto.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 from PIL import Image
 
 # ------------ CONFIG GPIO ------------
-BUTTON_PIN = 17
+BUTTON_PIN = 27  # <--- AHORA GPIO27
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -16,29 +16,23 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cap.set(cv2.CAP_PROP_FPS, 30)
 
-print("Listo. Presiona el botón físico para tomar foto.")
+print("Sistema listo en GPIO27. Pulsa el botón para tomar foto.")
 
 # ------------ LOOP ------------
 while True:
-    
-    if GPIO.input(BUTTON_PIN) == 1:
-        print("Obteniendo valor")
-    
-    # Botón presionado → GPIO17 pasa a LOW
-    if GPIO.input(BUTTON_PIN) == 0:
+    if GPIO.input(BUTTON_PIN) == 0:  # LOW = botón presionado
         print("Botón detectado → tomando foto...")
 
-        # Limpia frames viejos (logitech deja frames en buffer)
+        # limpiar buffer viejo de la camara
         for _ in range(6):
             cap.read()
 
-        ret, bgr = cap.read()
-        if ret:
+        ok, bgr = cap.read()
+        if ok:
             rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
             Image.fromarray(rgb).save("foto_rgb.png")
-            print("Foto guardada como foto_rgb.png")
+            print("Foto guardada: foto_rgb.png")
         else:
             print("Error leyendo la cámara")
 
-        time.sleep(0.4)  # Anti-rebote
-
+        time.sleep(0.4)  # anti-rebote
